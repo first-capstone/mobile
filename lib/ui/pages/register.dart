b909a451a014c829db02fa3336ab665466fe52a0
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-05-25 22:29:55 +0900

    :lipstick: 회원가입 UI 제작

--- lib/ui/pages/register.dart
+++ lib/ui/pages/register.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:flutter/material.dart';
+
 import 'package:union/utils/form_checker.dart';
 
 class UnionRegisterPage extends StatefulWidget {
@@ -71,12 +72,435 @@ class _UnionRegisterPageState extends State<UnionRegisterPage> {
               "assets/images/logo_blue_no.png",
               width: 30.w,
             ),
-            SizedBox(height: 5.h),
+            SizedBox(height: 3.h),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userIDTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorIDText = validateID(_userIDController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userIDController,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: const FaIcon(
+                            FontAwesomeIcons.solidUser,
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "아이디",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "아이디 입력",
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromARGB(
+                              _userIDController.text != "" &&
+                                      errorIDText == null
+                                  ? 0xFF
+                                  : 0x88,
+                              118,
+                              143,
+                              248),
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: SizedBox(
+                        child: Center(
+                          child: Text(
+                            "중복 확인",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.sp,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorIDText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userPasswordTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorPasswordText =
+                        validatePassword(_userPasswordController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userPasswordController,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: const FaIcon(
+                            FontAwesomeIcons.lock,
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "비밀번호",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "비밀번호 입력",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorPasswordText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userPasswordConfirmTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorPasswordConfirmText = validatePasswordConfirm(
+                        _userPasswordController.text,
+                        _userPasswordConfirmController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userPasswordConfirmController,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: const FaIcon(
+                            FontAwesomeIcons.lock,
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "비밀번호 재입력",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "비밀번호를 다시 입력",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorPasswordConfirmText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userNicknameTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorNicknameText =
+                        validateNickname(_userNicknameController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userNicknameController,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: const FaIcon(
+                            FontAwesomeIcons.tags,
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "닉네임",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "닉네임 입력",
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromARGB(
+                              _userNicknameController.text != "" &&
+                                      errorNicknameText == null
+                                  ? 0xFF
+                                  : 0x88,
+                              118,
+                              143,
+                              248),
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: SizedBox(
+                        child: Center(
+                          child: Text(
+                            "중복 확인",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.sp,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorNicknameText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userPhoneTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorPhoneText = validatePhone(_userPhoneController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userPhoneController,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: FaIcon(
+                            FontAwesomeIcons.mobileScreenButton,
+                            size: Adaptive.sp(25),
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "전화번호",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "000-0000-0000",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorPhoneText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+            Container(
+              width: 80.w,
+              padding: EdgeInsets.only(left: 4.w, right: 2.w),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userEmailTextFormKey,
+                onChanged: () {
+                  setState(() {
+                    errorEmailText = validateEmail(_userEmailController.text);
+                  });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _userEmailController,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: -1.w),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          icon: const FaIcon(
+                            FontAwesomeIcons.solidEnvelope,
+                          ),
+                          iconColor: Colors.grey[500],
+                          labelText: "이메일",
+                          floatingLabelStyle: TextStyle(
+                            fontSize: 18.sp,
+                          ),
+                          hintText: "20240101@example.ac.kr",
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromARGB(
+                              _userEmailController.text != "" &&
+                                      errorEmailText == null
+                                  ? 0xFF
+                                  : 0x88,
+                              118,
+                              143,
+                              248),
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: SizedBox(
+                        child: Center(
+                          child: Text(
+                            "메일 인증",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.sp,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Text(
+                errorEmailText ?? "",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
             TextButton(
               onPressed: () {},
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all<Color>(
-                  const Color.fromARGB(255, 118, 143, 248),
+                  Color.fromARGB(
+                      _userIDController.text != "" &&
+                              _userPasswordController.text != "" &&
+                              _userPasswordConfirmController.text != "" &&
+                              _userNicknameController.text != "" &&
+                              _userPhoneController.text != "" &&
+                              _userEmailController.text != "" &&
+                              errorIDText == null &&
+                              errorPasswordText == null &&
+                              errorPasswordConfirmText == null &&
+                              errorNicknameText == null &&
+                              errorPhoneText == null &&
+                              errorEmailText == null
+                          ? 0xFF
+                          : 0x88,
+                      118,
+                      143,
+                      248),
                 ),
                 shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
--- lib/utils/form_checker.dart
+++ lib/utils/form_checker.dart
@@ -17,3 +17,48 @@ String? validatePassword(String? value) {
     return null;
   }
 }
+
+String? validatePasswordConfirm(String? valueOld, String? valueConfirm) {
+  if (valueOld != valueConfirm) {
+    return "비밀번호가 일치하지 않습니다.";
+  } else if (validatePassword(valueConfirm) != null) {
+    return validatePassword(valueConfirm);
+  } else {
+    return null;
+  }
+}
+
+String? validateNickname(String? value) {
+  if (value == null || value.isEmpty || value.trim() == "") {
+    return "이 항목을 입력해주세요.";
+  } else if (value.length < 3 || value.length > 12) {
+    return "닉네임의 길이는 3자 이상, 12자 이하 입니다.";
+  } else {
+    return null;
+  }
+}
+
+String? validatePhone(String? value) {
+  if (value == null || value.isEmpty || value.trim() == "") {
+    return "이 항목을 입력해주세요.";
+  } else if (value.length != 13) {
+    return "전화번호는 13자 입니다.";
+  } else {
+    return null;
+  }
+}
+
+String? validateEmail(String? value) {
+  RegExp exp = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  if (!exp.hasMatch(value ?? "")) {
+    return "이메일 형식이 옳지 않습니다.";
+  } else if (value == null || value.isEmpty || value.trim() == "") {
+    return "이 항목을 입력해주세요.";
+  } else if (value.length >= 30) {
+    return "이메일 길이는 30자 미만입니다.";
+  } else {
+    return null;
+  }
+}
