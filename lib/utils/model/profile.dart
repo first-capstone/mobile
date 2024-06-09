e0b736b1c120feafd7178bb5097810c6ea50b4e6
aroxu <30624945+aroxu@users.noreply.github.com>
2024-06-10 03:00:32 +0900

    :building_construction: 사용자 프로필 모델 구현

---
+++ lib/utils/model/profile.dart
@@ -0,0 +1,12 @@
+class Profile {
+  String username;
+  String school;
+
+  String avatarUrl;
+
+  Profile({
+    required this.username,
+    required this.school,
+    required this.avatarUrl,
+  });
+}
